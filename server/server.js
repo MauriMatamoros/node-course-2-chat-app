623b1c5e4f41e2bd2d78fb417b1afa3d6559b734
MauriMatamoros <mauricio.enrique.matamoros@gmail.com>
2017-02-16 19:09:38 -0600

    Greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -14,7 +14,16 @@ app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
   console.log('New user connected');
-
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the Chat App',
+    createdAt: new Date().getTime()
+  });
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New user joined',
+    createdAt: new Date().getTime()
+  });
   // socket.emit('newEmail', {//emit
   //   from: 'mauricio@example.com',
   //   text: 'Hello World',
@@ -34,6 +43,11 @@ io.on('connection', (socket) => {
       text: message.text,
       createdAt: new Date().getTime()
     });
+    // socket.broadcast.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // });
   });
 
   // socket.on('createEmail', (newEmail) => {//listen
