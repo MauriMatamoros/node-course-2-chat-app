5c6c6f5e90d9054d1e6c1aba1653c7f903a090b5
MauriMatamoros <mauricio.enrique.matamoros@gmail.com>
2017-02-20 18:02:47 -0600

    Add mustache.js for message templates

--- public/js/index.js
+++ public/js/index.js
@@ -2,48 +2,34 @@ var socket = io();
 
 socket.on('connect', function (){
   console.log('Connected to server');
-  // socket.emit('createEmail', {//emit
-  //   to: 'carlos@example.com',
-  //   text: 'Hello World! 2',
-  // });
-//   socket.emit('createMessage', {
-//     from: 'Mauri',
-//     text: 'New message for you'
-//   })
 });
 
 socket.on('disconnect', function (){
   console.log('Disconnected from server');
 });
 
-// socket.on('newEmail', function(email){//listen
-//   console.log('New email');
-//   console.log(email);
-// });
 socket.on('newMessage', function (message){
-  // console.log('newMessage', message);
   var formattedTime = moment(message.createdAt).format('h:mm a');
-  var li = jQuery('<li></li>');
-  li.text(`${message.from} ${formattedTime}: ${message.text}`);
-  jQuery('#messages').append(li);
+  var template = jQuery('#message-template').html();
+  var html = Mustache.render(template, {
+    text: message.text,
+    from: message.from,
+    createdAt: formattedTime
+  });
+
+  jQuery('#messages').append(html);
 });
 
 socket.on('newLocationMessage', function (message) {
-  var formattedTime = moment(message.createMessage).format('h:mm a');
-  var li = jQuery('<li></li>');
-  var a = jQuery('<a target="_blank">My current location</a>');
-  li.text(`${message.from} ${formattedTime}: `);
-  a.attr('href', message.url);
-  li.append(a);
-  jQuery('#messages').append(li);
+  var formattedTime = moment(message.createdAt).format('h:mm a');
+  var template = jQuery('#location-message-template').html();
+  var html = Mustache.render(template, {
+    from: message.from,
+    url: message.url,
+    createdAt: formattedTime
+  });
+  jQuery('#messages').append(html);
 });
-
-// socket.emit('createMessage', {
-//   from: 'Mauri',
-//   text: 'Hello there!'
-// }, function (data) {
-//   console.log('Got it', data);
-// });
 
 jQuery('#message-form').on('submit', function (e) {
   e.preventDefault();
